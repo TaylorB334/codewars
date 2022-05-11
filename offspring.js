78f6d8f861ff6d572185c4bef08a55fa962d3a74
TaylorBarnard <tayy_@hotmail.co.uk>
2022-05-11 09:50:36 +0100

    simple function to return offspring sex based on input of genes

---
+++ offspring.js
@@ -0,0 +1,7 @@
+function chromosomeCheck(sperm) {
+    if (sperm === 'XY'){
+      return "Congratulations! You're going to have a son."
+    } else if (sperm === 'XX'){
+      return "Congratulations! You're going to have a daughter."
+    }
+  }
