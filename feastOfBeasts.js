a1a06963a436db1b276060cee230fc2bdc46b351
TaylorBarnard <tayy_@hotmail.co.uk>
2022-05-14 10:57:58 +0100

    check first and last letters of 2 strings match

---
+++ feastOfBeasts.js
@@ -0,0 +1,7 @@
+function feast(beast, dish) {
+    if (beast.charAt(0) === dish.charAt(0) && beast.charAt(text.length-1) === dish.charAt(text.length-1)){
+      return true
+    }else{
+      return false
+    }
+    }
