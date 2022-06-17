df5a56f481812be8b8240579e8fa5c65cdae76e2
TaylorBarnard <tayy_@hotmail.co.uk>
2022-06-17 19:37:15 +0100

    return the type of sum

---
+++ type-of-sum.js
@@ -0,0 +1,9 @@
+function typeOfSum(a, b) {
+    if (typeof a === 'string' || typeof b === 'string') {
+      return 'string'
+    }else if (typeof a === 'number' || typeof b === 'number'){
+      return 'number'
+    }else{
+      return 'number'
+    }
+  }
